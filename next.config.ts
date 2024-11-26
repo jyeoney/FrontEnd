ccae33bf6afa4d76e8c8f0c142a03ac713a0b4be
Sim0102 <recycle0102@gmail.com>
2024-11-26 17:12:43 +0900

    next config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "upgrade-insecure-requests; default-src 'self' 'unsafe-inline' 'unsafe-eval' http: https: data: blob: ws:",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
