37555fab3fe48029ffaeb1ddae720e9f60843bf3
Sim0102 <recycle0102@gmail.com>
2024-11-26 17:28:23 +0900

    webRTC 추가 수정

--- next.config.ts
+++ next.config.ts
@@ -13,10 +13,16 @@ const nextConfig: NextConfig = {
               "default-src 'self' 'unsafe-inline' 'unsafe-eval' http: https: data: blob: ws: wss:",
               "connect-src 'self' ws: wss: http: https:",
               "media-src 'self' http: https: data: blob:",
-              "child-src 'self' blob:",
+              "child-src 'self' blob: https://vercel.live",
               "worker-src 'self' blob:",
-              "frame-src 'self' blob:",
+              "frame-src 'self' blob: https://vercel.live",
+              "frame-ancestors 'self' https://vercel.live",
             ].join('; '),
+          },
+          {
+            // CORS 헤더 추가
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
           },
         ],
       },
