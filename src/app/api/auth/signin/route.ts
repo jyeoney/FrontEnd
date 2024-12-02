1a4de24bc6eb5192d6bf87a6bd1eb989c017ac5f
jyeoney <jiyeon2y2000@gmail.com>
2024-12-02 14:27:55 +0900

    feat: 토큰 유효기간 설정

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -1,51 +1,66 @@
 import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
+import jwt from 'jsonwebtoken';
 
 export const POST = async (req: NextRequest) => {
   const { email, password } = await req.json();
 
   try {
-    // if (email === 'user@example.com' && password === 'password123') {
-    //   // 토큰 생성 (실제 사용 시 JWT나 외부 인증 서비스를 사용)
-    //   const accessToken = 'exampleAccessToken';
-    //   const refreshToken = 'exampleRefreshToken';
-
-    //   const res = NextResponse.json({ message: '로그인 성공' });
-    // }
     const response = await axios.post(
-      '/api/auth/sign-in',
+      `${process.env.API_BASE_URL}/auth/sign-in`, // 실제 백엔드 API 경로
       { email, password },
       {
         headers: { 'Content-Type': 'application/json' },
-        // withCredentials: true
+        // withCredentials: true // 서버에서 보낸 쿠키를 클라이언트에서 자동으로 받아서 저장
       },
     );
 
     if (response.status === 200) {
       const { accessToken, refreshToken } = response.data;
 
+      const decodedAccessToken = jwt.decode(accessToken) as { exp: number };
+      if (!decodedAccessToken || !decodedAccessToken.exp) {
+        throw new Error('액세스토큰 exp 클레임이 없습니다.');
+      }
+
+      const currentTime = Math.trunc(Date.now() / 1000);
+      const accessTokenMaxAge = decodedAccessToken.exp - currentTime;
+
+      const decodedRefreshToken = jwt.decode(refreshToken) as { exp: number };
+      if (!decodedRefreshToken || !decodedRefreshToken.exp) {
+        throw new Error('리프레시토큰 exp 클레임이 없습니다.');
+      }
+
+      const refreshTokenMaxAge = decodedRefreshToken.exp - currentTime;
+
       const res = NextResponse.json({ message: '로그인 성공' });
 
       const cookies = res.cookies;
       cookies.set('accessToken', accessToken, {
         httpOnly: true, // 클라이언트 자바스크립트에서는 접근 불가(보안상의 이유)
-        //secure: process.env.NODE_ENV === 'production', // https 일때만 전송
+        secure: process.env.NODE_ENV === 'production', // https 일때만 전송
         path: '/', // 쿠키의 유효 경로
         sameSite: 'strict', // CSRF 보호
+        maxAge: accessTokenMaxAge, // 액세스토큰 유효기간
       });
       cookies.set('refreshToken', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         path: '/',
         sameSite: 'strict',
-        maxAge: 60 * 60 * 24 * 3, // 3일 동안 유효
+        maxAge: refreshTokenMaxAge, // 리프레시토큰 유효기간
       });
 
       return res;
     }
 
-    return NextResponse.json({ message: '로그인 실패' }, { status: 401 });
-  } catch (error) {
+    return NextResponse.json(response.data, { status: response.status });
+  } catch (error: any) {
+    if (axios.isAxiosError(error) && error.response) {
+      return NextResponse.json(error.response.data, {
+        status: error.response.status,
+      });
+    }
     return NextResponse.json({ message: '서버 오류' }, { status: 500 });
   }
 };
