19ee735a915ca5c8d74b6e6c0fbef914727d4a40
jyeoney <jiyeon2y2000@gmail.com>
2024-12-06 13:24:40 +0900

    feat: 미들웨어에서 액세스 토큰 재발급 처리 추가 및 특정 경로에서 미들웨어 실행 제외

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,26 +2,38 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  // 미들웨어 실행하지 않는 경우
+  if (
+    // request.nextUrl.pathname === '/signin' ||
+    request.nextUrl.pathname === '/api/mock/auth/sign-in'
+  ) {
+    return NextResponse.next();
+  }
+
   console.log('미들웨어 실행됨:', request.nextUrl.pathname);
   const accessToken = request.cookies.get('accessToken')?.value;
+  const refreshToken = request.cookies.get('refreshToken')?.value;
 
-  // 로그 출력으로 미들웨어가 정상적으로 동작하는지 확인
-  console.log('미들웨어 실행 - 요청 경로:', request.nextUrl.pathname);
-
-  // 인증이 필요한 경로를 보호
   if (accessToken) {
-    console.log('AccessToken 존재:', accessToken);
+    console.log('accessToken 존재:', accessToken);
     return NextResponse.next({
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
   }
-  // 기본적으로 요청을 계속 진행
-  console.log('AccessToken 없음');
-  return NextResponse.next();
+
+  if (refreshToken) {
+    console.log('accessToken 없음');
+    const refreshRoute = new URL('/api/mock/auth/token-reissue', request.url);
+    return NextResponse.rewrite(refreshRoute);
+  }
+
+  console.log('accesstoken, refreshToken 모두 없음');
+  const signInUrl = new URL('/signin', request.url);
+  return NextResponse.redirect(signInUrl);
 }
 
 export const config = {
-  matcher: ['/api/:path*'], // 특정 경로에서만 동작
+  matcher: ['/api/:path*'],
 };
