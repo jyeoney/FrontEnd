f657a075395ddd2268e2de1fe0bc52fc2a966fd9
Sim0102 <recycle0102@gmail.com>
2024-12-17 21:08:48 +0900

    small fix

--- src/app/api/users/[userId]/profile-image/route.ts
+++ src/app/api/users/[userId]/profile-image/route.ts
@@ -59,8 +59,8 @@ export const DELETE = async (
     );
 
     if (response.status === 200) {
-      const { isActive, createdAt, updatedAt, ...updatedUserInfo } =
-        response.data;
+      const { id, nickname, email, profileImageUrl } = response.data;
+      const updatedUserInfo = { id, nickname, email, profileImageUrl };
       return NextResponse.json(updatedUserInfo, { status: response.status });
     }
   } catch (error: any) {
