9542281e011d55d44b10f7cd3fd3cba561e24499
Sim0102 <recycle0102@gmail.com>
2024-12-20 16:09:03 +0900

    fix config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['swany-bucket.s3.ap-northeast-2.amazonaws.com'], // 외부 이미지 도메인 추가
+    domains: ['devonoffbucket.s3.ap-northeast-2.amazonaws.com'], // 외부 이미지 도메인 추가
   },
   async headers() {
     return [
